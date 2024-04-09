871c5cf15cce9be03f396c70e24adf2f6d4fcd24
Amune victor <viktorefi2001@gmail.com>
2024-04-09 14:32:22 +0000

    reaction timer

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h1>Ninja reaction timer</h1>
+  <button @click="start" :disabled="isplaying">Play</button>
+
+  <Block  v-if="isplaying" :delay="delay" @end="endGame"/>
+  <Result :score="score" v-if="showResults"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Results.vue'
+export default {
+  name: 'App',
+  components: {Block, Result},
+   data (){
+    return {
+     isplaying: false,
+     delay:null,
+     score: null,
+     showResults:false
+    }
+   },
+  methods: {
+    start (){
+      this.isplaying = true,
+      this.delay =  2000 + Math.random()*5000
+      this.showResults = false
+    },
+    endGame (reactionTime){
+       this.score = reactionTime
+       this.isplaying = false
+       this.showResults = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity:0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p class="score">Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+ props: ['score'],
+
+ data () {
+    return {
+        rank: null
+    }
+ },
+
+ mounted(){
+    if(this.score < 250){
+        this.rank = "Ninja fingers"
+    }else if (this.score < 400){
+        this.rank = "Rapid relexes"
+    }else{
+        this.rank ="Snail pace......"
+    }
+ }
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    font-weight: bold;
+    color: #0faf87;
+}
+</style>
